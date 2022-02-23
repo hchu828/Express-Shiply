05349046977e80f2dbb4f3a7928ab320b53097c3
hectorchu~ <hector.chu828@gmail.com>
2022-02-22 18:17:03 -0800

    Added JSON schema and mock functions

--- routes/shipments.js
+++ routes/shipments.js
@@ -4,18 +4,27 @@ const express = require("express");
 const router = new express.Router();
 
 const { shipProduct } = require("../shipItApi");
+const { BadRequestError } = require("../expressError");
+const jsonschema = require("jsonschema");
+const orderSchema = require("../schemas/order.json")
 
 /** POST /ship
  *
  * VShips an order coming from json body:
- *   { productId, name, addr, zip }
+ *   { productId, name, addr, zip } TODO: just pass in req.body
  *
  * Returns { shipped: shipId }
  */
 
 router.post("/", async function (req, res, next) {
-  const { productId, name, addr, zip } = req.body;
-  const shipId = await shipProduct({ productId, name, addr, zip });
+
+  const result = jsonschema.validate(req.body, orderSchema);
+  if (!result.valid) {
+    let errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const shipId = await shipProduct(req.body);
   return res.json({ shipped: shipId });
 });
 
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -1,11 +1,18 @@
 "use strict";
 
+const shipItApi = require("../shipItApi");
+
+shipItApi.shipProduct = jest.fn();
+
 const request = require("supertest");
 const app = require("../app");
 
 
+
 describe("POST /", function () {
   test("valid", async function () {
+    shipItApi.shipProduct.mockReturnValue(1000);
+
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
       name: "Test Tester",
@@ -13,6 +20,25 @@ describe("POST /", function () {
       zip: "12345-6789",
     });
 
-    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    expect(resp.body).toEqual({ shipped: 1000 });
+  });
+
+  test("invalid request", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 999,
+      name: 999,
+      addr: "100 Test St",
+      zip: "12345-6789",
+    });
+
+    expect(resp.body).toEqual({
+      error: {
+        message: [
+          "instance.productId must be greater than or equal to 1000",
+          "instance.name is not of a type(s) string"
+        ],
+        status: 400
+      }
+    });
   });
 });
